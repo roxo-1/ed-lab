fed9518426e1741162d514d0baf10c4b87f03bb0
Carolina Lee <111212023+roxo-1@users.noreply.github.com>
2025-02-14 11:58:44 +0000

    exercicio 2

--- App.java
+++ App.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
 
-public class App{
-    public static void main(String[] args){
+public class SomaSimples {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Digite seu nome: ");
-        String nome = scanner.nextLine();
-        System.out.println("Bem-vindo, " + nome + "!");
+        System.out.print("Digite um número: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Digite outro número: ");
+        int num2 = scanner.nextInt();
+        int soma = num1 + num2;
+        System.out.println("A soma é: " + soma);
         scanner.close();
-    }
+ }
 }
