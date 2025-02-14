50fb3488bfcfda73b16e03a3703476da8827a34a
Carolina Lee <111212023+roxo-1@users.noreply.github.com>
2025-02-14 11:59:32 +0000

    exercicio 3

--- App.java
+++ App.java
@@ -4,11 +4,12 @@ public class SomaSimples {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Digite um número: ");
-        int num1 = scanner.nextInt();
-        System.out.print("Digite outro número: ");
-        int num2 = scanner.nextInt();
-        int soma = num1 + num2;
-        System.out.println("A soma é: " + soma);
+        int num = scanner.nextInt();
+        if (num % 2 == 0) {
+        System.out.println("O número " + num + " é par.");
+        } else {
+        System.out.println("O número " + num + " é ímpar.");
+        }
         scanner.close();
  }
 }
